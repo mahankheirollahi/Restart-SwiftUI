59d14d2ab1421eb4b451c598821fd2040cf57b49
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-07-03 21:02:19 +0300

    Initial Commit

---
+++ Restart-SwiftUI/Restart_SwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Restart_SwiftUIApp.swift
+//  Restart-SwiftUI
+//
+//  Created by Mahan Kheirollahi on 7/3/24.
+//
+
+import SwiftUI
+
+@main
+struct Restart_SwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
