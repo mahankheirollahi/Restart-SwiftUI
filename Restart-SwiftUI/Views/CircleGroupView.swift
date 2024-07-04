f636e9abfe0777134402392f29d0b58bdfcd3d98
Mahan Kheirollahi <kheirollahi.mahan@gmail.com>
2024-07-05 00:18:05 +0300

    Add animation

--- Restart-SwiftUI/Views/CircleGroupView.swift
+++ Restart-SwiftUI/Views/CircleGroupView.swift
@@ -12,15 +12,27 @@ struct CircleGroupView: View {
     
     @State var ShapeColor: Color
     @State var ShapeOpacity: Double
+    @State private var isAnimating: Bool = false
     
     var body: some View {
-        Circle()
-            .stroke(ShapeColor.opacity(ShapeOpacity),lineWidth: 40)
+        ZStack {
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity),lineWidth: 40)
             .frame(width: 260,height: 260,alignment: .center)
+            
+            Circle()
+                .stroke(ShapeColor.opacity(ShapeOpacity),lineWidth: 80)
+                .frame(width: 260,height: 260,alignment: .center)
+        }
+        .blur(radius: isAnimating ? 0 : 10)
+        .opacity(isAnimating ? 1 : 0)
+        .scaleEffect(isAnimating ? 1 : 0.5)
+        .animation(.easeOut(duration: 1),value: isAnimating)
+        .onAppear(perform: {
+            isAnimating = true
+        })
         
-        Circle()
-            .stroke(ShapeColor.opacity(ShapeOpacity),lineWidth: 80)
-            .frame(width: 260,height: 260,alignment: .center)
+       
     }
 }
 
